e45b2372d829329214345555802e314bd0313bd8
Andrew Malchuk <andrew.malchuk@yandex.ru>
2020-09-07 18:07:24 +0400

    Mon, 07 Sep 2020 14:07:24 +0000

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="scienco",
-    version="0.1.4",
+    version="0.1.5b0",
     author="Andrew Malchuk",
     author_email="andrew.malchuk@yandex.ru",
     description="Calculate the readability of text using one of a variety of computed indexes",
@@ -15,7 +15,7 @@ setup(
     url="https://gitlab.com/amalchuk/scienco",
     license="MIT",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Education",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
